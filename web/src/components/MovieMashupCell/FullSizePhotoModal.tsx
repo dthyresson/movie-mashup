8273532778f976c8c6e99c1676496541271aae76
David Thyresson <dthyresson@gmail.com>
2024-08-12 08:55:23 -0400

    dismiss

--- web/src/components/MovieMashupCell/FullSizePhotoModal.tsx
+++ web/src/components/MovieMashupCell/FullSizePhotoModal.tsx
@@ -1,17 +1,29 @@
-// Add this new component
-export const FullSizePhotoModal = ({ photo, onClose }) => (
-  <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75">
-    <div className="relative max-h-[90vh] max-w-[90vw]">
-      <img
-        src={photo.imageUrl}
-        alt="Full size mashup"
-        className="max-h-[90vh] max-w-[90vw] object-contain" />
-      <button
-        onClick={onClose}
-        className="absolute right-2 top-2 rounded-full bg-white p-2 text-black hover:bg-gray-200"
-      >
-        💥
-      </button>
+export const FullSizePhotoModal = ({ photo, onClose }) => {
+  const handleClose = (e: React.MouseEvent | React.KeyboardEvent) => {
+    if (
+      e.target === e.currentTarget ||
+      (e as React.KeyboardEvent).key === 'Escape'
+    ) {
+      onClose()
+    }
+  }
+
+  return (
+    // eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions
+    <div
+      role="dialog"
+      aria-modal="true"
+      className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75"
+      onClick={handleClose}
+      onKeyDown={handleClose}
+    >
+      <div className="relative max-h-[90vh] max-w-[90vw]">
+        <img
+          src={photo.imageUrl}
+          alt="Full size mashup"
+          className="max-h-[90vh] max-w-[90vw] object-contain"
+        />
+      </div>
     </div>
-  </div>
-);
+  )
+}

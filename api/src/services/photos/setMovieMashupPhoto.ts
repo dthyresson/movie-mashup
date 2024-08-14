485d9d1240d7d641a72a59df0e9d7049a308bbcb
David Thyresson <dthyresson@gmail.com>
2024-08-14 10:10:30 -0400

    invalidate

--- api/src/services/photos/setMovieMashupPhoto.ts
+++ api/src/services/photos/setMovieMashupPhoto.ts
@@ -1,8 +1,8 @@
 import { SetMovieMashupPhotoResolver } from 'types/setMovieMashupPhoto'
 
+import { cache } from 'src/functions/graphql'
 import { db } from 'src/lib/db'
 import { movieMashup } from 'src/services/movieMashups/movieMashups'
-
 export const setMovieMashupPhoto: SetMovieMashupPhotoResolver = async ({
   input,
 }) => {
@@ -14,6 +14,11 @@ export const setMovieMashupPhoto: SetMovieMashupPhotoResolver = async ({
     where: { id: input.photoId },
     include: { movieMashup: true },
   })
+
+  cache.invalidate([
+    { typename: 'MovieMashup', id: input.movieMashupId },
+    { typename: 'Photo', id: input.photoId },
+  ])
 
   return await movieMashup({ id: input.movieMashupId })
 }
